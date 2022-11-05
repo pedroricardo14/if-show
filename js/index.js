340b6aef71101ac593aa86f3f177d0349b16524a
Pedro Ricardo <115710304+pedroricardo14@users.noreply.github.com>
2022-11-05 10:43:06 -0300

    Adicionando interação ao menu mobile

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
+const btnMobile = document.getElementById('btn-mobile');
 
+function toggleMenu(event){
+    if (event.type === 'touchstart') event.preventDefault() 
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    
+event.currentTarget.setAttribute('aria-expanded', 'true');
+    if (active){
+     event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else{
+        
+event.currentTarget.setAttribute('aria-label', 'abrir menu');
+    }
+    
+    
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
